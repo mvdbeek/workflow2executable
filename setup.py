3fc128da5765b545ec7188b19f029585f857c67b
mvdbeek <m.vandenbeek@gmail.com>
2020-07-05 13:43:34 +0200

    Set minimum bioblend version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'bioblend']
+requirements = ['Click>=6.0', 'bioblend>=0.14.0']
 
 setup_requirements = ['pytest-runner', ]
 
